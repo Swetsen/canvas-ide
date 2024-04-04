9e3dc879c4f0934a2fd005be305ade9fb2d69d9b
Swetsen <madado.dorn@gmail.com>
2024-04-04 21:01:52 +0200

    Page Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProjectSelector from "./ProjectSelector";
+import IDE from "./IDE";
 
 function App() {
   return (
-    <>
-    <h1>
-      Hello World!
-    </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={ProjectSelector} />
+        <Route path="/project/:id" element={IDE} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/ProjectSelector.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProjectSelector() {
+  return (
+    <>
+      <h1>Canvas IDE</h1>
+      <h2>Projects</h2>
+    </>
+  );
+}
+
+export default ProjectSelector;
